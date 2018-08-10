9995f2f8c272d60b9fd05f3977949658c7bf58a7
杭城小刘 <704568245@qq.com>
2018-08-10 17:37:58 +0800

    Web 端反爬虫技术第一版

---
+++ Demo/Spider-develop/Solution/Solution1.js
@@ -0,0 +1,31 @@
+(function ($) {	
+
+	function loadData(){
+		var url = "http://192.168.0.116:3000/api/solution1";
+		$.ajax({
+			type: "GET",
+			async: true,
+			url: url,
+			data: JSON.stringify({ "key": "gwwwh222" }),
+			success: function (json) {
+				if (!jQuery.isEmptyObject(json)) {
+					var data = json.data;
+					var log = data.analysis;
+					$("#name").text(data.name);
+					$("#year").html(getRawData(data.year,log));
+					$("#month").html(getRawData(data.month,log));
+					$("#day").html(getRawData(data.day,log));
+				}
+			},
+			error: function (XMLHttpRequest, textStatus, errorThrown) {
+				console.log("请求失败，请检查网络后重试");
+			}
+		});
+	}
+
+	$.Solution1 = function (arg) {
+		setTimeout(() => {
+			loadData();
+		}, 1000);
+	}
+})(jQuery);
